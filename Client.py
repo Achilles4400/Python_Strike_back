2fb4d6ac4c9d325f3efe89885f5ea9785b0582bb
Achilles4400 <v.bathellier@hotmail.fr>
2014-04-24 16:02:36 +0200

    creation des fonction task et api

--- Client.py
+++ Client.py
@@ -1,13 +1,24 @@
 __author__ = 'Vincent Bathellier'
 import http.client
 
+
+#fonction qui récupére un stript à  éxecuter
+def task():
+    script = input("Entrez le script à éxécuter : ")
+    file = open(script).read()
+    print(file)
+
+#fonction qui prend en paramètre une autre fonction
+def api(taskToExecut):
+    return taskToExecut()
+
+#fonction qui récupére les adresse IP
 def OpenIP(filename):
     file = open(filename,"r")
     for lignes in file:
-        print("ligne :")
-        print(lignes)
         ask(lignes)
 
+#fonction qui à pour but récupérer la charge des serveurs
 def ask(addIP):
     askServ = http.client.HTTPConnection(addIP, 1025 )
     askServ.connect()
