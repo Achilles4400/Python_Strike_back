0ac1058136c8e00c9d27e4653c865e922b8b7887
Achilles4400 <v.bathellier@hotmail.fr>
2014-04-25 15:34:36 +0200

    modif de ask api et fonc

--- Client.py
+++ Client.py
@@ -2,32 +2,36 @@ __author__ = 'Vincent Bathellier'
 import http.client
 from celery import Celery
 
+fileIP = input("Entrez le fichier d'adresse IP : ")
+host = OpenIP(fileIP)
+tache = Celery(taskToExecut, broker='amqp://guest:'+host+':1024//')
+
 #fonction qui récupére un stript à  éxecuter
-def task():
+@tache.task
+def fonc():
     script = input("Entrez le script à éxécuter : ")
     file = open(script).read()
-    print(file)
 
 #fonction qui prend en paramètre une autre fonction et qui retourne le resultat
-def api(taskToExecut):
-    fileIP = input("Entrez le fichier d'adresse IP : ")
-    host = OpenIP(fileIP)
-    app = Celery(taskToExecut, broker='amqp://guest:'+host+':1024//')
+def api(fonc):
+    result = fonc.delay(4,4)
+    return result.get()
 
 
 #fonction qui récupére les adresse IP et qui renvoi une adresse IP
 def OpenIP(filename):
+    global IPviable
     file = open(filename,"r")
     for lignes in file:
-        IP = lignes
+        IPviable = lignes
         charge = ask(lignes)
-    return IP
+    return IPviable
 
 #fonction qui à pour but récupérer la charge des serveurs
 def ask(addIP):
     askServ = http.client.HTTPConnection(addIP, 1025 )
     askServ.connect()
-    askServ.request('GET',requete)
+    askServ.request('GET','index.html')
     chargeServ = askServ.getresponse()
-    if chargeServ.status == httpOK:
-         return(chargeServ.read())
+    if chargeServ.status == 200: #200 correspond au status OK
+         return (chargeServ.read())
